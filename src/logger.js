450fafc8b4afc360565c81cd7bddf8400c8a1005
abdullahrao12080 <abdullahrao12080@gmail.com>
2026-07-31 19:29:45 +0200

    Src/logger.js correct path initiated after crration of scr

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const logFile = path.join(__dirname, "logs", "bot.log");
+const logFile = path.join(__dirname, "..", "data", "logs", "bot.log");
 
 function log(message) {
     const timestamp = new Date().toLocaleString();
